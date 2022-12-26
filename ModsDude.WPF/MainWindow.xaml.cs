7a1c0ca9a1813ba2596f7ececeb878f39d1529fe
Anton Johnsson Håkansson <anton.jh@hotmail.com>
2022-12-26 17:42:22 +0100

    Cleanup and renaming

--- ModsDude.WPF/MainWindow.xaml.cs
+++ ModsDude.WPF/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ModsDude2.WPF;
+namespace ModsDude.WPF;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
